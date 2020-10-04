a73002105607391da2d7c6022d9e66cbd3d85818
Mureed Sultan <MureedSultan@users.noreply.github.com>
2020-10-04 19:31:37 -0400

    Added Webcam Component

---
+++ macro/pages/video.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+const video = () => {
+  const webcamRef = React.useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width="100%"
+      />
+      <button onClick={capture}>Capture photo</button>
+    </div>
+  );
+};
+
+export default video;
